4970d2aa52f5719552728949b243cb80483b08ce
suminkim0 <ksm7058@naver.com>
2024-07-12 18:20:32 +0900

    로그인 작업 1

--- front-app/src/components/Header.js
+++ front-app/src/components/Header.js
@@ -1,8 +1,37 @@
+import { Link } from "react-router-dom";
+import styles from '../css/header.module.css';
+import logo from '../img/logo.png'; // 로고 경로 설정
+
 export default function Header() {
-  
-  return(
-    <div>
-      
+  return (
+    <div className={styles.headerContainer}>
+      <header className={styles.header}>
+        <div className={styles.auth}>
+          <ul>
+            <li>
+              <Link to="/register" className={styles.authItem}>회원가입</Link>
+            </li>
+            <li>
+              <Link to="/login" className={styles.authItem}>로그인</Link>
+            </li>
+          </ul>
+        </div>
+        <nav className={styles.nav}>
+          <ul className={styles.navList}>
+            <li>
+              <Link to="/">
+                <img src={logo} alt="Logo" className={styles.logo} />
+              </Link>
+            </li>
+            <li className={styles.navItem}><Link to="/">동물 의료</Link></li>
+            <li className={styles.navItem}><Link to="/">놀이 · 편의</Link></li>
+            <li className={styles.navItem}><Link to="/">자랑 게시판</Link></li>
+            <li className={styles.navItem}><Link to="/">산책 친구 찾기</Link></li>
+            <li className={styles.navItem}><Link to="/">동물보호</Link></li>
+            <li className={styles.navItem}><Link to="/">관리자문의</Link></li>
+          </ul>
+        </nav>
+      </header>
     </div>
   );
 }
---
+++ front-app/src/components/Login.js
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+export default function Login() {
+  const [id, setId] = useState('');
+  const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:9999/login', { id, pass: password }, { withCredentials: true });
+      if (response.data === 'success') {
+        console.log('로그인 요청 성공');
+        navigate('/');
+      } else {
+        alert('로그인 실패: ' + response.data);
+      }
+    } catch (error) {
+      alert('로그인 실패: ' + error.message);
+      console.error(error);
+    }
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h2>회원 로그인</h2>
+        <input
+          type="text"
+          placeholder="아이디 입력"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          required
+        />
+        <input
+          type="password"
+          placeholder="비밀번호 입력"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <div>
+          <button type="submit">로그인</button>
+          <a onClick={() => navigate('/register')}>회원가입</a>
+        </div>
+      </form>
+    </div>
+  );
+}
