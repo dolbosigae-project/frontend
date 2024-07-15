74fd6ad39a3184d1ea1be7edfac57bd29e4d6ed5
sumin <ksm7058@naver.com>
2024-07-15 22:27:37 +0900

    회원삭제 기능 추가

--- front-app/src/components/MemberView.js
+++ front-app/src/components/MemberView.js
@@ -3,11 +3,12 @@ import logo_white from '../img/logo_white.png';
 import { useEffect, useRef, useState } from 'react';
 import axios from 'axios';
 import default_img from '../img/default_img.png';
-import { Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function MemberView() {
   const [memberList, setMemberList] = useState([]);
   const [pagination, setPagination] = useState(null);
+  const navigate = useNavigate();
   let txtId = useRef();
   let txtName = useRef();
   let txtRegion = useRef();
@@ -38,6 +39,16 @@ export default function MemberView() {
   if (memberList.length === 0) {
     return <div>회원 데이터를 로딩중입니다.</div>
   }
+
+  const handleInputChange = (e, memberId, field) => {
+    const updatedList = memberList.map(member => {
+      if (member.boardMemberId === memberId) {
+        return { ...member, [field]: e.target.value };
+      }
+      return member;
+    });
+    setMemberList(updatedList);
+  };
 
   const handlePetWithChange = (e, memberId) => {
     const updatedList = memberList.map(member => {
@@ -82,15 +93,17 @@ export default function MemberView() {
     fetchPageData();
   };
 
-  const deleteClick = () => {
-    axios.delete('http://localhost:9999/member/delete',{
-      params : {id : txtId.current.value}
-  })
-    .then(response => {
-      console.log(response);
-      Navigate('/');
-    })
-  }
+  const deleteClick = async (memberId) => {
+    try {
+      const response = await axios.delete(`http://localhost:9999/member/delete/${memberId}`);
+      alert(response.data); // 서버 응답 메시지를 알림으로 표시
+      // 상태 업데이트: 삭제된 멤버를 목록에서 제거
+      setMemberList(memberList.filter(member => member.boardMemberId !== memberId));
+    } catch (error) {
+      console.error("There was an error deleting the member!", error);
+      alert("회원정보 삭제 중 오류가 발생했습니다.");
+    }
+  };
 
   return (
     <div>
@@ -105,7 +118,7 @@ export default function MemberView() {
           <option>지역</option>
           <option>등급</option>
         </select>
-        <input/>
+        <input />
         <button>검색</button>
       </div>
       <div className={styles.manage_table_container}>
@@ -124,18 +137,20 @@ export default function MemberView() {
                 <td>
                   <input type='checkbox' />
                   <img src={member.petImagePath || default_img} alt="Pet" className={styles.pet_image} /><br />
-                  <span ref={txtId}>{member.boardMemberId}</span>
+                  <span>{member.boardMemberId}</span>
                 </td>
                 <td>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>이름</span> <input value={member.boardMemberName} ref={txtName}/><br />
+                    <span className={styles.label}>이름</span>
+                    <input value={member.boardMemberName} ref={txtName} onChange={(e) => handleInputChange(e, member.boardMemberId, 'boardMemberName')} /><br />
                   </div>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>지역</span> <input value={member.boardMemberRegion} ref={txtRegion}/><br />
+                    <span className={styles.label}>지역</span>
+                    <input value={member.boardMemberRegion} ref={txtRegion} onChange={(e) => handleInputChange(e, member.boardMemberId, 'boardMemberRegion')} /><br />
                   </div>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>등급</span> 
-                    <select className={styles.customSelect} value={member.boardMemberGradeName} ref={txtGradeName} onChange={(e) => handleGenderChange(e, member.boardMemberId)}>
+                    <span className={styles.label}>등급</span>
+                    <select className={styles.customSelect} value={member.boardMemberGradeName} ref={txtGradeName} onChange={(e) => handleInputChange(e, member.boardMemberId, 'boardMemberGradeName')}>
                       <option value="애견인">애견인</option>
                       <option value="관리자">관리자</option>
                       <option value="관련기업">관련기업</option>
@@ -144,42 +159,45 @@ export default function MemberView() {
                 </td>
                 <td>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>유무</span> 
+                    <span className={styles.label}>유무</span>
                     <select className={styles.customSelect} ref={txtPetWith} value={member.boardMemberPetWith ? '있음' : '없음'} onChange={(e) => handlePetWithChange(e, member.boardMemberId)}>
                       <option value="있음">있음</option>
                       <option value="없음">없음</option>
                     </select><br />
                   </div>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>이름</span> <input value={member.boardMemberNick} ref={txtNick }/><br />
+                    <span className={styles.label}>이름</span>
+                    <input value={member.boardMemberNick} ref={txtNick} onChange={(e) => handleInputChange(e, member.boardMemberId, 'boardMemberNick')} /><br />
                   </div>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>출생년월</span> <input value={member.petBirth} ref={txtPetBirth}/>
+                    <span className={styles.label}>출생년월</span>
+                    <input value={member.petBirth} ref={txtPetBirth} onChange={(e) => handleInputChange(e, member.boardMemberId, 'petBirth')} />
                   </div>
                 </td>
                 <td>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>성별</span> 
+                    <span className={styles.label}>성별</span>
                     <select className={styles.customSelect} value={member.petGender} ref={txtPetGender} onChange={(e) => handleGenderChange(e, member.boardMemberId)}>
                       <option value="F">F</option>
                       <option value="M">M</option>
                     </select><br />
                   </div>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>무게분류</span> 
-                    <select className={styles.customSelect} value={member.petSize} ref={txtPetSize} onChange={(e) => handleGenderChange(e, member.boardMemberId)}>
+                    <span className={styles.label}>무게분류</span>
+                    <select className={styles.customSelect} value={member.petSize} ref={txtPetSize} onChange={(e) => handleInputChange(e, member.boardMemberId, 'petSize')}>
                       <option value="대형견">대형견</option>
                       <option value="중형견">중형견</option>
                       <option value="소형견">소형견</option>
                     </select><br />
                   </div>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>무게(kg)</span> <input value={member.petWeight} ref={txtPetWeight}/>
+                    <span className={styles.label}>무게(kg)</span>
+                    <input value={member.petWeight} ref={txtPetWeight} onChange={(e) => handleInputChange(e, member.boardMemberId, 'petWeight')} />
                   </div>
                 </td>
                 <td>
                   <div className={styles.flex_container}>
-                    <span className={styles.label}>산책프로필 노출 여부</span> 
+                    <span className={styles.label}>산책프로필 노출 여부</span>
                     <select className={styles.customSelect} ref={txtPetWalkProfile} value={member.petWalkProfile ? '예' : '아니오'} onChange={(e) => handleWalkProfileChange(e, member.boardMemberId)}>
                       <option value="예">예</option>
                       <option value="아니오">아니오</option>
@@ -187,12 +205,12 @@ export default function MemberView() {
                   </div>
                   <div className={styles.flex_container}>
                     <span className={styles.label}>소개글</span><br />
-                    <input value={member.petInfo} className={styles.petInfo} ref={txtPetInfo}/>
+                    <input value={member.petInfo} className={styles.petInfo} ref={txtPetInfo} onChange={(e) => handleInputChange(e, member.boardMemberId, 'petInfo')} />
                   </div>
                 </td>
                 <td>
                   <button className={styles.update}>수정</button><br />
-                  <button className={styles.delete} onClick={deleteClick}>삭제</button>
+                  <button className={styles.delete} onClick={() => deleteClick(member.boardMemberId)}>삭제</button>
                 </td>
               </tr>
             ))}
