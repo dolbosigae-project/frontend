dfb25314c695f022dddd2d5b57c64109781304ea
kwj7561 <kwj7561@naver.com>
2024-07-17 18:22:44 +0900

    0717

--- front-app/src/components/ShelterList.js
+++ front-app/src/components/ShelterList.js
@@ -1,4 +1,3 @@
-// src/ShelterList.js
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
@@ -7,12 +6,15 @@ const ShelterList = () => {
     const [shelters, setShelters] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [selectedRegion, setSelectedRegion] = useState('선택');
+    const [filteredShelters, setFilteredShelters] = useState([]);
 
     useEffect(() => {
         const fetchShelters = async () => {
             try {
                 const response = await axios.get('https://nam3324.synology.me:32800/shelter');
                 setShelters(response.data);
+                setFilteredShelters(response.data);  // 처음 로드 시 전체 쉼터 정보를 설정합니다.
             } catch (error) {
                 setError(error);
             } finally {
@@ -22,6 +24,19 @@ const ShelterList = () => {
 
         fetchShelters();
     }, []);
+
+    const handleFilterChange = (event) => {
+        setSelectedRegion(event.target.value);
+    };
+
+    const filterShelters = () => {
+        if (selectedRegion === '선택') {
+            setFilteredShelters(shelters); // 선택 항목인 경우 전체 쉼터 정보를 설정합니다.
+        } else {
+            const filtered = shelters.filter(shelter => shelter.shRegion === selectedRegion);
+            setFilteredShelters(filtered);
+        }
+    };
 
     if (loading) {
         return <div>Loading...</div>;
@@ -34,6 +49,34 @@ const ShelterList = () => {
     return (
         <div>
             <h1>Shelter List</h1>
+            <div>
+                <select value={selectedRegion} onChange={handleFilterChange}>
+                    <option>선택</option>
+                    <option>가평군</option>
+                    <option>고양시</option>
+                    <option>과천시</option>
+                    <option>광명시</option>
+                    <option>광주시</option>
+                    <option>구리시</option>
+                    <option>남양주시</option>
+                    <option>동두천시</option>
+                    <option>부천시</option>
+                    <option>성남시</option>
+                    <option>수원시</option>
+                    <option>시흥시</option>
+                    <option>안산시</option>
+                    <option>양주시</option>
+                    <option>양평군</option>
+                    <option>연천군</option>
+                    <option>용인시</option>
+                    <option>의왕시</option>
+                    <option>의정부시</option>
+                    <option>평택시</option>
+                    <option>하남시</option>
+                    <option>화성시</option>
+                </select>
+                <button onClick={filterShelters}>조회</button>
+            </div>
             <table>
                 <thead>
                     <tr>
@@ -44,12 +87,12 @@ const ShelterList = () => {
                     </tr>
                 </thead>
                 <tbody>
-                    {shelters.map((shelter, index) => (
+                    {filteredShelters.map((shelter) => (
                         <tr key={shelter.shID}>
                             <td>{shelter.shRegion}</td>
                             <td>
                                 <Link to={`/shelter/${shelter.shID}`}>
-                                {shelter.shName}
+                                    {shelter.shName}
                                 </Link>
                             </td>
                             <td>{shelter.shTel}</td>
