94bfee5ddd9602442a3ed6e92728204506e9ea88
suminkim0 <ksm7058@naver.com>
2024-07-19 16:31:16 +0900

    카카오 로그인 3

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -8,6 +8,7 @@ import MemberView from './components/MemberView';
 import MemberRegister from './components/MemberRegister';
 import MyPage from './components/MyPage';
 import LoginPasswd from './components/LoginPasswd';
+import KakaoLogin from './components/KakaoLogin';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -32,12 +33,13 @@ function App() {
       <div>
         <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
         <Routes>
-          <Route path="/" element={<Home/>} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login onLoginSuccess={handleLoginSuccess} />} />
-          <Route path="/login/passwd" element={<LoginPasswd/>} />
-          <Route path="/member/view" element={<MemberView/>} />
-          <Route path="/member/register" element={<MemberRegister/>} />
-          <Route path="/member/mypage" element={<MyPage/>} />
+          <Route path="/login/passwd" element={<LoginPasswd />} />
+          <Route path="/member/view" element={<MemberView />} />
+          <Route path="/member/register" element={<MemberRegister />} />
+          <Route path="/member/mypage" element={<MyPage />} />
+          <Route path="/kakao/callback" element={<KakaoLogin onLoginSuccess={handleLoginSuccess} />} />
         </Routes>
       </div>
     </Router>
