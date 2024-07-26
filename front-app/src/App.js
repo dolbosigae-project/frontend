d3d7937f6c81be8a38377a24c450833be5d54452
kwj7561 <kwj7561@naver.com>
2024-07-26 15:41:42 +0900

    0726

--- front-app/src/App.js
+++ front-app/src/App.js
@@ -9,14 +9,30 @@ import MemberRegister from './components/MemberRegister';
 import MyPage from './components/MyPage';
 import LoginPasswd from './components/LoginPasswd';
 import KakaoLogin from './components/KakaoLogin';
-import PL from './components/pl_main_components/contents/PL'
-import PlInfoView from './components/pl_info_component/PlInfoView'
+import PL from './components/pl_main_components/contents/PL';
+import PlInfoView from './components/pl_info_component/PlInfoView';
 import ShelterList from './components/ShelterList';
-import ShelterDetail from './components/ShelterDetail';  // ShelterDetail import 추가
+import ShelterAnimalsInfo from './components/ShelterAnimalsInfo';
 import Hospital from './components/Hospital';
 import HospitalDetail from './components/HospitalDetail';
-import ABList from './components/ABList';
-import ABDetail from './components/ABDetail';
+import AdminContact from './components/AdminContact';
+import AdminContactNormalTableDetail from './components/AdminContactNormalTableDetail';
+import AdminContactWrite from './components/AdminContactWrite';
+import Footer from './components/Footer';
+import IoChat from './components/IoChat';
+import ChatIntro from './components/ChatIntro';
+import MateSearch from './components/MateSearch';
+import MatePetProfile from './components/MatePetProfile';
+import Pharmacy from './components/Pharmacy';
+import PharmacyDetail from './components/PharmacyDetail';
+import PlInsert from './components/pl_insert_component/PlInsert';
+import CoInsert from './components/co_insert_component/CoInsert';
+import DogWorldCup from './components/dog_worldCup_components/DogWorldCup';
+import AddHospital from './components/AddHospital';
+import AddPharmacy from './components/AddPharmacy';
+import CO from './components/co_main_components/CO';
+import CoInfoView from './components/co_info_components/CoInfoView';
+import DogRandomDate from './components/DogRandomDate';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
@@ -30,7 +46,7 @@ function App() {
 
   const handleLoginSuccess = () => {
     setIsLoggedIn(true);
-  }
+  };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
@@ -38,13 +54,48 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <Routes>
-          <Route path='/shelters' element={<ShelterList />} />
-          <Route path='/shelters/detail/:shID' element={<ShelterDetail />} /> 
-          <Route path='/ab' element={<ABList />} />
-          <Route path='/ab/detail/:abID' element={<ABDetail/>} />
-        </Routes>
+      <div className="app-container">
+        <Header isLoggedIn={isLoggedIn} onLogout={handleLogout} />
+        <main className="app-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login onLoginSuccess={handleLoginSuccess} />} />
+            <Route path="/login/passwd" element={<LoginPasswd />} />
+            <Route path="/member/view" element={<MemberView />} />
+            <Route path="/member/register" element={<MemberRegister />} />
+            <Route path="/member/mypage" element={<MyPage />} />
+            <Route path="/kakao/callback" element={<KakaoLogin onLoginSuccess={handleLoginSuccess} />} />
+            <Route path="/pl" element={<PL />} />
+            <Route path='/plinfo/:plId' element={<PlInfoView />} />
+            <Route path='/plInsert' element={<PlInsert />} />
+            <Route path='/co' element={<CO />} />
+            <Route path='/coinfo/:coId' element={<CoInfoView />} />
+            <Route path='/coInsert' element={<CoInsert />} />
+            <Route path='/shelter' element={<ShelterList />} />
+            <Route path='/shelter/animal' element={<ShelterAnimalsInfo />} />
+            <Route path='/animal-medical' element={<Hospital />} />
+            <Route path='/hoinfo/:hoId' element={<HospitalDetail />} />
+            <Route path="/pharmacies" element={<Pharmacy />} />
+            <Route path="/phinfo/:phId" element={<PharmacyDetail />} />
+            <Route path='/dwc' element={<DogWorldCup />} />
+            <Route path='/hospitalDetail' element={<HospitalDetail />} />
+            <Route path='/admin/contact' element={<AdminContact />} />
+            <Route path='/admin/contact/detail/:adminNo' element={<AdminContactNormalTableDetail />} />
+            <Route path='/admin/write' element={<AdminContactWrite />} />
+            <Route path='/dog/random/date' element={<DogRandomDate />} />
+            <Route path='/co' element={<CO />} />
+            <Route path='/coinfo/:coId' element={<CoInfoView />} />
+            <Route path='/pharmacies' element={<Pharmacy />} />
+            <Route path='/phinfo/:phId' element={<PharmacyDetail />} />
+            <Route path="/addHospital" element={<AddHospital />} />
+            <Route path="/addPharmacy" element={<AddPharmacy />} />
+            <Route path="/mate/chat" element={<IoChat/>} />
+            <Route path="/mate/intro" element={<ChatIntro/>} />
+            <Route path="/mate/member" element={<MateSearch/>} />
+            <Route path="/mate/petinfo" element={<MatePetProfile/>} />
+          </Routes>
+        </main>
+        <Footer />
       </div>
     </Router>
   );
