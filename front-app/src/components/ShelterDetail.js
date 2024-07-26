734882d1437f52977715806ab1c92e7f16624dc9
kwj7561 <kwj7561@naver.com>
2024-07-26 18:55:39 +0900

    0726

--- front-app/src/components/ShelterDetail.js
+++ front-app/src/components/ShelterDetail.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams, Link } from 'react-router-dom';
-import ABList from './ABList'; // ABList 컴포넌트 import
+import { useParams } from 'react-router-dom';
+import ABList from './ABList';
 import styles from '../css/ShelterDetail.module.css';
 
 const ShelterDetail = () => {
@@ -15,6 +15,7 @@ const ShelterDetail = () => {
             setLoading(true);
             try {
                 const response = await axios.get(`http://localhost:9999/shelters/detail/${shID}`);
+                console.log(response);
                 setShelter(response.data);
             } catch (error) {
                 setError(error);
@@ -37,18 +38,37 @@ const ShelterDetail = () => {
     return (
         <div className={styles.shelter_detail}>
             <h1>셸터 상세 정보</h1>
-            <div className={styles.shelter_info}>
-                <p><strong>센터명:</strong> {shelter?.shName}</p>
-                <p><strong>연락처:</strong> {shelter?.shTel}</p>
-                <p><strong>해당지역:</strong> {shelter?.shRegion}</p>
-                <p><strong>주소:</strong> {shelter?.shAddress}</p>
-                <p><strong>운영시간:</strong> {shelter?.shHour}</p>
-                <p><strong>보호동물종:</strong> {shelter?.shAnimalType}</p>
-            </div>
+            <table className={styles.shelter_info_table}>
+                <tbody>
+                    <tr>
+                        <td><strong>센터명:</strong></td>
+                        <td>{shelter?.shName}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>연락처:</strong></td>
+                        <td>{shelter?.shTel}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>해당지역:</strong></td>
+                        <td>{shelter?.shRegion}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>주소:</strong></td>
+                        <td>{shelter?.shAddress}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>운영시간:</strong></td>
+                        <td>{shelter?.shHour}</td>
+                    </tr>
+                    <tr>
+                        <td><strong>보호동물종:</strong></td>
+                        <td>{shelter?.shAnimalType}</td>
+                    </tr>
+                </tbody>
+            </table>
             <h2>보호 동물 목록</h2>
             <ABList shID={shID} />
         </div>
     );
-};
-
+}
 export default ShelterDetail;
