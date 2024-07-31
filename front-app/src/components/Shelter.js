c9bc4c2068f12888058ce1446f2376058c4e016c
sumin <ksm7058@naver.com>
2024-07-31 23:06:16 +0900

    shelter3 css

--- front-app/src/components/Shelter.js
+++ front-app/src/components/Shelter.js
@@ -1,28 +1,25 @@
 import axios from 'axios';
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import SubTitleShelter from './SubTitles/SubTitleShelter';
 import styles from '../css/shelter.module.css';
-
 import { Link } from 'react-router-dom';
 
 export default function Shelter() {
-  const [shelterList, setShelterList] = useState([]); //빈배열로 초기화
+  const [shelterList, setShelterList] = useState([]);
   const [pagination, setPagination] = useState(null);
-  const [searchCategory, setSearchCategory] = useState('지역선택'); // 검색 기준 상태 변수
-  const [searchTerm, setSearchTerm] = useState(''); // 검색어 상태 변수
+  const [searchCategory, setSearchCategory] = useState('지역선택');
+  const [searchTerm, setSearchTerm] = useState('');
 
-  useEffect(()=>{
+  useEffect(() => {
     const readData = async () => {
-      try{
+      try {
         const response = await axios.get('http://localhost:9999/shelter/list');
-        console.log('Pagination Data:', response.data.pagination);
         setShelterList(response.data.shelter);
         setPagination(response.data.pagination);
-        console.log(response.data);
-      } catch(error){
+      } catch (error) {
         console.error("보호센터 조회 중 에러발생", error);
       }
-    }
+    };
     readData();
   }, []);
 
@@ -35,47 +32,41 @@ export default function Shelter() {
       } catch (error) {
         console.error("보호센터 다른 페이지 조회 중 에러발생", error);
       }
-    }
+    };
     fetchPageData();
   };
 
   const handleSearch = async () => {
     try {
-      console.log('Search Category:', searchCategory);
-      console.log('Search Term:', searchTerm);
       const response = await axios.get('http://localhost:9999/shelter/search', {
         params: {
           category: searchCategory,
           term: searchTerm
         }
       });
-      console.log('Search response:', response.data); // 응답 데이터 로깅
-
       if (response.data && Array.isArray(response.data)) {
-        setShelterList(response.data); // 데이터가 배열이면 설정
-        console.log('Updated shelterList:', response.data);
+        setShelterList(response.data);
       } else {
-        setShelterList([]); // 데이터가 배열이 아니면 빈 배열로 설정
+        setShelterList([]);
       }
-
       if (response.data && response.data.pagination) {
         setPagination(response.data.pagination);
       } else {
-        setPagination(null); // 페이지네이션 데이터가 없으면 null로 설정
+        setPagination(null);
       }
     } catch (error) {
       console.error("지역 센터 검색 중 오류 발생!", error);
       alert("센터 검색 중 오류가 발생했습니다.");
-      setShelterList([]); // 오류 발생 시 센터 리스트 초기화
-      setPagination(null); // 오류 발생 시 페이지네이션 초기화
+      setShelterList([]);
+      setPagination(null);
     }
   };
 
-  return(
+  return (
     <div>
       <SubTitleShelter />
       <div className={styles.searchBox}>
-        <select value={searchCategory} onChange={(e)=> setSearchCategory(e.target.value)}>
+        <select value={searchCategory} onChange={(e) => setSearchCategory(e.target.value)}>
           <option>지역선택</option>
           <option>경기도 가평군</option>
           <option>경기도 고양시</option>
@@ -98,16 +89,15 @@ export default function Shelter() {
           <option>경기도 의정부시</option>
           <option>경기도 평택시</option>
           <option>경기도 하남시</option>
-          <option>경기도 화성시 </option>
+          <option>경기도 화성시</option>
         </select>
         <input value={searchTerm} placeholder='센터명 입력' onChange={(e) => setSearchTerm(e.target.value)} />
         <button onClick={handleSearch}>검색</button>
       </div>
       {shelterList.length === 0 ? (
         <div>해당 데이터가 없습니다.</div>
-      ) : ( 
-        <table>
-          <thead>
+      ) : (
+        <table className={styles.table}>
             <tr>
               <th>지역</th>
               <th>이름</th>
@@ -115,42 +105,36 @@ export default function Shelter() {
               <th>전화번호</th>
               <th>운영시간</th>
             </tr>
-          </thead>
-          <tbody>
-            {shelterList.map((shelter)=>(
+            {shelterList.map((shelter) => (
               <tr key={shelter.shelterId}>
                 <td>{shelter.shelterRegion}</td>
                 <td>
-                  <Link to={`/shelter/detail/${shelter.shelterId}`}>{shelter.shelterName}</Link>
+                  <Link to={`/shelter/detail/${shelter.shelterId}`} className={styles.link}>{shelter.shelterName}</Link>
                 </td>
                 <td>{shelter.shelterAddress}</td>
                 <td>{shelter.shelterTel}</td>
                 <td>{shelter.shelterHour}</td>
               </tr>
             ))}
-            
-          </tbody>
-          <tfoot>
-              <tr>
-                <td colSpan="6" className={styles.pagination}>
-                  {pagination && pagination.previousPageGroup && (
-                    <button onClick={() => handlePageChange(pagination.startPageOfPageGroup - 1)}>◀</button>
-                  )}
-                  {pagination && Array.from({ length: pagination.endPageOfPageGroup - pagination.startPageOfPageGroup + 1 }, (_, i) => (
-                    <button
-                      key={i + pagination.startPageOfPageGroup}
-                      onClick={() => handlePageChange(i + pagination.startPageOfPageGroup)}
-                      className={pagination.currentPage === i + pagination.startPageOfPageGroup ? styles.activePage : ''}
-                    >
-                      {i + pagination.startPageOfPageGroup}
-                    </button>
-                  ))}
-                  {pagination && pagination.nextPageGroup && (
-                    <button onClick={() => handlePageChange(pagination.endPageOfPageGroup + 1)}>▶</button>
-                  )}
-                </td>
-              </tr>
-            </tfoot>
+            <tr>
+              <td className={styles.paginationGroup} colSpan={5} style={{ textAlign: 'center' }}>
+                {pagination && pagination.previousPageGroup && (
+                  <button onClick={() => handlePageChange(pagination.startPageOfPageGroup - 1)}>◀</button>
+                )}
+                {pagination && Array.from({ length: pagination.endPageOfPageGroup - pagination.startPageOfPageGroup + 1 }, (_, i) => (
+                  <button
+                    key={i + pagination.startPageOfPageGroup}
+                    onClick={() => handlePageChange(i + pagination.startPageOfPageGroup)}
+                    className={pagination.currentPage === i + pagination.startPageOfPageGroup ? styles.activePage : ''}
+                  >
+                    {i + pagination.startPageOfPageGroup}
+                  </button>
+                ))}
+                {pagination && pagination.nextPageGroup && (
+                  <button onClick={() => handlePageChange(pagination.endPageOfPageGroup + 1)}>▶</button>
+                )}
+              </td>
+            </tr>
         </table>
       )}
     </div>
--- front-app/src/components/ShelterDetail.js
+++ front-app/src/components/ShelterDetail.js
@@ -31,36 +31,38 @@ export default function ShelterDetail() {
   return (
     <div>
       <SubTitleShelter />
-      {shelterDetail.shelterLatitude === 0 ? (
-        <div>지도가 제공되지 않습니다</div>
-      ) : (
-        <KakaoMap locations={[location]} />
-      )}
-      {shelterDetail ? (
-        <table>
-          <thead>
-            <tr>
-              <th>지역</th>
-              <th>이름</th>
-              <th>주소</th>
-              <th>전화번호</th>
-              <th>운영시간</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>{shelterDetail.shelterRegion}</td>
-              <td>{shelterDetail.shelterName}</td>
-              <td>{shelterDetail.shelterAddress}</td>
-              <td>{shelterDetail.shelterTel}</td>
-              <td>{shelterDetail.shelterHour}</td>
-            </tr>
-          </tbody>
-        </table>
-      ) : (
-        <div>해당 데이터가 없습니다.</div>
-      )}
-      <Link to='/shelter'>글 목록</Link>
+      <div className={styles.container}>
+        {shelterDetail.shelterLatitude === 0 ? (
+          <div className={styles.noMapMessage}>지도가 제공되지 않습니다</div>
+        ) : (
+          <KakaoMap locations={[location]} />
+        )}
+        {shelterDetail ? (
+          <table className={styles.table}>
+            <thead>
+              <tr>
+                <th>지역</th>
+                <th>이름</th>
+                <th>주소</th>
+                <th>전화번호</th>
+                <th>운영시간</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>{shelterDetail.shelterRegion}</td>
+                <td>{shelterDetail.shelterName}</td>
+                <td>{shelterDetail.shelterAddress}</td>
+                <td>{shelterDetail.shelterTel}</td>
+                <td>{shelterDetail.shelterHour}</td>
+              </tr>
+            </tbody>
+          </table>
+        ) : (
+          <div>해당 데이터가 없습니다.</div>
+        )}
+        <Link to='/shelter' className={styles.link}>글 목록</Link>
+      </div>
     </div>
   );
 }
